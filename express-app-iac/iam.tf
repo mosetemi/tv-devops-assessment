c3ae6be478f8771340d31c585dfea3fbdd41f3bd
Temi <temidayomoses87@hotmail.com>
2026-06-19 02:58:35 -0400

    Add IAM roles and policies for ECS task execution and management

---
+++ express-app-iac/iam.tf
@@ -0,0 +1,30 @@
+data "aws_iam_policy_document" "ecs_task_assume_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    effect  = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "ecs_execution_role" {
+  name               = "${local.name_prefix}-ecs-execution-role"
+  assume_role_policy = data.aws_iam_policy_document.ecs_task_assume_role.json
+
+  tags = merge(local.standard_tags, { Name = "${local.name_prefix}-ecs-execution-role" })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_execution_role_policy" {
+  role       = aws_iam_role.ecs_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_iam_role" "ecs_task_role" {
+  name               = "${local.name_prefix}-ecs-task-role"
+  assume_role_policy = data.aws_iam_policy_document.ecs_task_assume_role.json
+
+  tags = merge(local.standard_tags, { Name = "${local.name_prefix}-ecs-task-role" })
+}
