621220f78189013c79e9b65bda161a4b276e1c8f
Temi <temidayomoses87@hotmail.com>
2026-06-22 03:41:06 -0400

    Final push

--- express-app-iac/backend.tf
+++ express-app-iac/backend.tf
@@ -2,7 +2,7 @@ terraform {
 # Remote backend config template. When running, I used my own S3 bucket and DynamoDB table for state locking.
 # You will need to create your own and update the values below.
     backend "s3" {
-        bucket = "turbovets-test"
+        bucket = "YOUR_BUCKET_NAME"
         key = "express-ts-app/terraform.tfstate"
         region = "us-east-1"
         encrypt = true
