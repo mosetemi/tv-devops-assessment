cac3293a29652d93870c6e6f114e302824958743
Temi <temidayomoses87@hotmail.com>
2026-06-22 01:31:38 -0400

    revert workflow on main branch

--- express-app-iac/backend.tf
+++ express-app-iac/backend.tf
@@ -1,6 +1,6 @@
 terraform {
-# Remote backend config template. When running, I used my own
-# ... S3 bucket and DynamoDB table for state locking. You will need to create your own and update the values below.
+# Remote backend config template. When running, I used my own S3 bucket and DynamoDB table for state locking.
+# You will need to create your own and update the values below.
     backend "s3" {
         bucket = "turbovets-test"
         key = "express-ts-app/terraform.tfstate"
